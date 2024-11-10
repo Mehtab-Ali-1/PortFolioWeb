6bb5695ecd67008d646d43e8d2e03e6d42cef1ec
Mehtab Ali <160261202+Mehtab-Ali-1@users.noreply.github.com>
2024-11-10 10:53:27 +0500

    Update page.tsx

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -12,7 +12,7 @@ export default function Portfolio () {
             <div className="md:h-72 w-full max-md:w-screen  bg-orange-100">
                 <Header />
                 <div className="md:text-4xl pt-20 font-bold  font-serif flex justify-center items-center">
-                    <ul className="flex space-x-3 max-md:pb-10 md:pt-16 text-lg items-center">
+                    <ul className="flex space-x-3 max-md:pb-10 md:pt-16 text-lg items-center select-none">
                         <li><Link href="/home">Home</Link></li>
                         <li><IoIosArrowForward />
                         </li>
@@ -22,13 +22,13 @@ export default function Portfolio () {
             </div>
             {/* &&&&&&&&&&&&&&&&&&&&&&&&&&&& */}
             {/* heading */}
-            <div className="text-3xl md:text-4xl font-serif text-blue-950 flex text-center justify-center pt-10 px-5">
+            <div className="text-3xl md:text-4xl font-serif text-blue-950 flex text-center justify-center pt-10 px-5 select-none">
                 <h1>Showcasing My Journey in Web Development</h1>
             </div>
 
             <div className="md:flex justify-evenly items-center mt-12">
             {/* pic1  */}
-                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8">
+                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8 select-none">
                     <Image className="imageSize rounded-t-xl shadow-slate-800 shadow-xl transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 " src="/cv.png" alt="CV" height={300} width={300}/>
                         <p className="text-xl font-semibold pt-6">My Resume</p>
                         <Link className=" text-lg bg-orange-500 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-orange-400" href="https://my-resume-w35x.vercel.app/">View</Link>
@@ -36,7 +36,7 @@ export default function Portfolio () {
                     </div>
                 </div>
             {/* pic2  */}
-                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8">
+                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8 select-none">
                     <Image className="imageSize rounded-t-xl shadow-slate-800 shadow-xl transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" src="/resumeBuilder.png" alt="CV" height={300} width={300}/>
                         <p className="text-xl font-semibold pt-6">Dynamic Resume Generator</p>
                         <Link className=" text-lg bg-orange-500 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-orange-400" href="https://resume-builder-qymw.vercel.app/">View</Link>
@@ -44,7 +44,7 @@ export default function Portfolio () {
                     </div>
                 </div>
             {/* pic3 */}
-                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8">
+                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8 select-none">
                     <Image className="imageSize rounded-t-xl shadow-slate-800 shadow-xl transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" src="/paymentForm.png" alt="CV" height={300} width={300}/>
                         <p className="text-xl font-semibold pt-6">Payment Form</p>
                         <Link className=" text-lg bg-orange-500 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-orange-400" href="https://payment-form-h34s.vercel.app/">View</Link>
@@ -53,10 +53,10 @@ export default function Portfolio () {
                 </div>
             </div>
             {/* ***************** */}
-        <div className="md:flex justify-center items-center space-x-5 px-7">
+        <div className="md:flex justify-center items-center space-x-5 px-7 select-none">
             {/* second web project */}
             <div>
-                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8  md:pt-16 px-5">
+                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8  md:pt-16 px-5 select-none">
                     <Image className="webSize rounded-t-xl shadow-slate-800 shadow-xl transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" src="/figmaWebTravel.png" alt="CV" height={300} width={400}/>
                         <p className=" relative text-center text-xl font-semibold pt-6">Travel Agency Website</p>
                         <Link className=" text-lg bg-orange-500 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-orange-400" href="https://trave-agency-web-vdp4.vercel.app/">View</Link>
@@ -66,7 +66,7 @@ export default function Portfolio () {
             </div>
             {/* Countdown Timer  */}
             <div>
-                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8  md:pt-16 px-5">
+                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8  md:pt-16 px-5 select-none">
                     <Image className="portfolioSize rounded-t-xl shadow-slate-800 shadow-xl transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" src="/countDownTimer.png" alt="CV" height={300} width={400}/>
                         <p className=" relative text-center text-xl font-semibold pt-6">Countdown Timer</p>
                         <Link className=" text-lg bg-orange-500 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-orange-400" href="https://count-down-timer-proj-c21z.vercel.app/">View</Link>
@@ -76,7 +76,7 @@ export default function Portfolio () {
             </div>
         </div>
         {/* calculator */}
-        <div className="md:flex justify-center items-center" >
+        <div className="md:flex justify-center items-center select-none" >
                 <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8  md:pt-16 px-5">
                     <Image className="portfolioSize rounded-t-xl shadow-slate-800 shadow-xl transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" src="/calculator.png" alt="CV" height={300} width={400}/>
                         <p className=" relative text-center text-xl font-semibold pt-6">Calculator</p>
@@ -85,7 +85,7 @@ export default function Portfolio () {
                     </div>
                 </div>
                 {/* tic toc toe game */}
-                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8  md:pt-16 px-5">
+                <div className=" flex flex-col items-center justify-center space-y-4 max-md:mb-8  md:pt-16 px-5 select-none">
                     <Image className="portfolioSize rounded-t-xl shadow-slate-800 shadow-xl transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" src="/tictactoe.png" alt="ticTacToe" height={300} width={400}/>
                         <p className=" relative text-center text-xl font-semibold pt-6">Tic Tac Toe Game</p>
                         <Link className=" text-lg bg-orange-500 px-6 py-2 rounded-md hover:bg-slate-800 hover:text-orange-400" href="https://tic-toc-toe-game-next-js.vercel.app/">View</Link>
